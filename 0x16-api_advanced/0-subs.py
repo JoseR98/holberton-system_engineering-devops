04d2e07963eaf3ac162bee8a8fda90f53ee99bf2
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-17 12:54:53 -0500

    Delete redundancy, conditional in the last line was useless

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,3 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
     return 0
-
-
-if __name__ == "__main__":
-    pass
